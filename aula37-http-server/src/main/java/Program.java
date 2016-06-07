b730c157ef2e264470e8375d2a3ebef9cfa41bdf
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-06-07 16:10:09 +0100

    Aula 37 TPc

--- aula37-http-server/src/main/java/Program.java
+++ aula37-http-server/src/main/java/Program.java
@@ -10,7 +10,7 @@ public class Program {
             new HttpServer()
                     .addHandler("/timev2", (req) -> String.format("Time is %s\nVersion:0.4.1", LocalTime.now().toString()))
                     .addHandler("/ola", (req) -> "isel " + Math.random())
-                    .addHandler("/aluno/*", (req) -> "aluno nr: ... " )
+                    .addHandler("/aluno/*", (req) -> "aluno nr: " + req.getPathInfo().substring(1) )
                     .run();
     }
 }
